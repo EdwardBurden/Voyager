fe57dc5b5e84b5898428d892fa574de40211dbba
EdwardBurden <edward.burden.hull@gmail.com>
2021-03-18 00:13:45 +0000

    about to start powercontroller

---
+++ Assets/Scripts/ShipPowerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPowerController : MonoBehaviour
+{
+	//use ipowerrequired to find powerrequired
+	//use ipowersupplier to find powersuppliers
+	//is a component powered? , can irequired find is upply and does it have enough left after every other irequired takes its amount
+	//total power generated
+	//have enough power?
+}
